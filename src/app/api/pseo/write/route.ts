a17e7421778414be0a3b75401f0b2f01964a4da2
Michael Yao <yao.developpeur@gmail.com>
2024-05-31 03:31:44 +0100

    updates

--- src/app/api/pseo/write/route.ts
+++ src/app/api/pseo/write/route.ts
@@ -5,6 +5,8 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 import { getSummary } from 'readability-cyr';
 
+export const maxDuration = 120;
+
 export async function POST(request: Request) {
   const body = await request.json();
   const ai = new AI()
