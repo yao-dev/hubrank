86d9108189ebeef140148aa1c69d1f68038dfef5
Michael Yao <yao.developpeur@gmail.com>
2024-06-01 17:11:00 +0100

    updates

---
+++ src/app/api/zapier/webhook/route.ts
@@ -0,0 +1,34 @@
+import { supabaseAdmin } from "@/helpers/supabase";
+import { NextResponse } from "next/server";
+
+const supabase = supabaseAdmin(process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY || "");
+
+export async function GET(request: Request) {
+  try {
+    const apiKey = request.headers.get("X-API-KEY")
+    return NextResponse.json({ apiKey })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    return NextResponse.json(body)
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
+
+export async function DELETE(request: Request) {
+  try {
+    const apiKey = request.headers.get("X-API-KEY")
+    return NextResponse.json({ apiKey })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
