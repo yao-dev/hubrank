49d4d25fa6caf51f3ea4c4af42e3a5876f0e9f4e
Michael Yao <yao.developpeur@gmail.com>
2024-06-03 01:08:33 +0100

    updates

--- src/app/api/zapier/webhook/trigger/publish/route.ts
+++ src/app/api/zapier/webhook/trigger/publish/route.ts
@@ -6,7 +6,20 @@ const supabase = supabaseAdmin(process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY || "")
 // When the user publish a blog
 // the user id is sent in the request body
 // use it to retrieve the zapier webhook url
-// once retrieved, send a request to the webhook url with the expected params
+// once retrieved, send a request to the webhook url
+// with an array containing the blog post payload (see xample below)
+// {
+//   "id": 345,
+//   "created_at": "2024-05-31 15:21:50.357459+00",
+//   "html": "",
+//   "markdown": "",
+//   "title": "",
+//   "seed_keyword": "",
+//   "keywords": "",
+//   "meta_description": "",
+//   "featured_image": "",
+//   "schema_markups": []
+// }
 export async function POST(request: Request) {
   try {
     const body = await request.json();
