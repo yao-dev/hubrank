55d4d322e8fb2a2de11e82b73c5a5837be181953
Michael Yao <yao.developpeur@gmail.com>
2024-07-15 15:01:23 +0100

    updates

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,12 +3,19 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import queryKeys from "@/helpers/queryKeys";
 import supabase from "@/helpers/supabase";
 import { getUserId } from "@/helpers/user";
+import { useEffect, useState } from "react";
 
 const useUser = () => {
+  const [userId, setUserId] = useState();
+
+  useEffect(() => {
+    getUserId().then((userId) => setUserId(userId)).catch(console.error)
+  }, []);
   const { data: user } = useQuery({
+    enabled: !!userId,
     queryKey: queryKeys.user(),
     queryFn: async () => {
-      return supabase.from('users').select('*').eq("id", await getUserId()).maybeSingle();
+      return supabase.from('users').select('*').eq("id", userId).maybeSingle();
     },
     select: ({ data }) => {
       return data;
