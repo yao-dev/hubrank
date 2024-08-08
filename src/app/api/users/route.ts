5a88109af79410159acffad92705d2df5d032a44
Michael Yao <yao.developpeur@gmail.com>
2024-08-08 23:47:15 +0200

    updates

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { supabaseAdmin } from "@/helpers/supabase";
 import { updateCredits } from "../helpers";
+import { upsertStripeCustomer } from "@/features/payment/helpers";
 
 const supabase = supabaseAdmin(process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY || "");
 export const maxDuration = 300;
@@ -11,6 +12,7 @@ export async function POST(request: Request) {
   try {
     switch (body.type) {
       case 'INSERT': {
+        await upsertStripeCustomer(body.record.id);
         await updateCredits({ userId: body.record.id, credits: 5, action: 'replace' })
         break;
       }
