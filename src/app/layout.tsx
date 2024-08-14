f9d5db86a4d353a9da1b94e582848083cf8fc06d
Michael Yao <yao.developpeur@gmail.com>
2024-08-14 17:54:26 +0200

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "
 
 const siteConfig = {
   name: "Hubrank",
-  short_description: '',
-  description: "",
+  short_description: "Generate SEO blogs, social media captions & replies in just few clicks.",
+  description: "Grow 10x Faster with AI Content Marketing for Your Business. Boost your SEO, Create Social Captions & Replies and more.",
   url,
   locale: "en_US",
   keywords: [],
