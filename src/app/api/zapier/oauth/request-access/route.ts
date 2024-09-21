66b1ec273034f268f16866ced41fd5e1d8b8caaa
Michael Yao <yao.developpeur@gmail.com>
2024-09-21 19:58:13 +0100

    updates

--- src/app/api/zapier/oauth/request-access/route.ts
+++ src/app/api/zapier/oauth/request-access/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import chalk from "chalk";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
     console.log(chalk.yellow("url received from zapier"), request.url)
-    const searchParams = new URLSearchParams(request.url);
-    const redirect_uri = searchParams.get("redirect_uri") ?? "";
+    console.log();
+    const redirect_uri = request.nextUrl.searchParams.get("redirect_uri") ?? "";
     console.log(chalk.yellow("redirect uri"), redirect_uri)
 
     const query = {
       install_zapier: true,
-      state: searchParams.get("state") ?? "",
+      state: request.nextUrl.searchParams.get("state") ?? "",
       client_id: process.env.NEXT_PUBLIC_ZAPIER_CLIENT_ID ?? "",
       redirect_uri,
     };
-    const urlEncoded = new URLSearchParams(query).toString();
+    const urlEncoded = Object.entries(query).map(([key, value]) => `${key}=${value}`).join("&");
     const protocol = process.env.NODE_ENV === "development" ? "http:" : "https:"
     const redirectUrl = `${protocol}//app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/projects?${urlEncoded}`;
 
